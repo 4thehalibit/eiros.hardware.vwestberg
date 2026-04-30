1a73c17130d1a62a21a2cf91a77f58c6b0d1535f
Vincent Westberg <4thehalibit@gmail.com>
2026-04-30 00:39:52 +0000

    add root filesystem

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -18,6 +18,10 @@
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
+  filesystems."/" = {
+    device = "/dev/disk/by-uuid/5a07a728-8308-4e0d-97c2-f94a51f5b023";
+    fsType = "ext4";
+    };
 
   swapDevices = [ ];
 
